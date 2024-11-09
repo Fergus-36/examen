273b0dc20e70eeaa3e08e0b187e7f8d6ecb93947
Fergus-36 <L24550380@chihuahua2.tecnm.mx>
2024-11-08 18:09:24 -0600

    Add files via upload

---
+++ 24550380_examen2/src/pkg24550380_examen2/Main.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg24550380_examen2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alondra
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+      */
+    public static void main(String[] args) {
+        Scanner captu = new Scanner(System.in);
+        String usua, random ;
+    String piedra, papel, tijera;
+       
+        System.out.println("piedra, papel, tijera:");
+            usua = captu.nextLine();
+   
+            switch (usua) {
+        
+                case piedra: 
+                    random=Math.random();
+}
+        //if () {}
+       
+    
+   
+    }
+    
+}
